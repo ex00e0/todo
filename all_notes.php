deedfbfed6f018b4ef2dc087ef891765b8a2db90
ex00e0 <148848809+ex00e0@users.noreply.github.com>
2024-09-22 21:23:37 +0300

    Add files via upload

--- all_notes.php
+++ all_notes.php
@@ -63,7 +63,7 @@ else {
             <input type="checkbox" checked value="false" name="checkbox" style="display:none;">
         </form>
         <div></div>
-        <div class="not_done_note"><?=$task[2]?></div>
+        <div class="not_done_note" onclick="getDesc(<?=$task[0]?>)"><?=$task[2]?></div>
         <svg class="edit" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" onclick="getEdit(<?=$task[0]?>)">
             <path d="M7.67272 3.99106L1 10.6637V14H4.33636L11.0091 7.32736M7.67272 3.99106L10.0654 1.59837L10.0669 1.59695C10.3962 1.26759 10.5612 1.10261 10.7514 1.04082C10.9189 0.986392 11.0993 0.986392 11.2669 1.04082C11.4569 1.10257 11.6217 1.26735 11.9506 1.59625L13.4018 3.04738C13.7321 3.37769 13.8973 3.54292 13.9592 3.73337C14.0136 3.90088 14.0136 4.08133 13.9592 4.24885C13.8974 4.43916 13.7324 4.60414 13.4025 4.93398L13.4018 4.93468L11.0091 7.32736M7.67272 3.99106L11.0091 7.32736" stroke="#CDCDCD" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
@@ -90,7 +90,7 @@ else {
             </svg>
         </form>
         <div></div>
-        <div class="done_note"><?=$task[2]?></div>
+        <div class="done_note" onclick="getDesc(<?=$task[0]?>)"><?=$task[2]?></div>
         <svg class="edit" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" onclick="getEdit(<?=$task[0]?>)">
             <path d="M7.67272 3.99106L1 10.6637V14H4.33636L11.0091 7.32736M7.67272 3.99106L10.0654 1.59837L10.0669 1.59695C10.3962 1.26759 10.5612 1.10261 10.7514 1.04082C10.9189 0.986392 11.0993 0.986392 11.2669 1.04082C11.4569 1.10257 11.6217 1.26735 11.9506 1.59625L13.4018 3.04738C13.7321 3.37769 13.8973 3.54292 13.9592 3.73337C14.0136 3.90088 14.0136 4.08133 13.9592 4.24885C13.8974 4.43916 13.7324 4.60414 13.4025 4.93398L13.4018 4.93468L11.0091 7.32736M7.67272 3.99106L11.0091 7.32736" stroke="#CDCDCD" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
@@ -130,9 +130,9 @@ else {
     <!-- <div class="vh17"></div> -->
     <form class="modal_block_new" method="post" id="modal">
          <div class="headline r2 c2" id="headline_modal">НОВАЯ ЗАДАЧА</div>
-         <input type="hidden" value="<?="<script>$('#search').val()</script>"?>" name="search" id="search_val">
-         <input type="hidden" value="<?="<script>$('#filter').val()</script>"?>" name="filter" id="filter_val">
-         <input type="hidden" value="<?="<script>$('#sort').val()</script>"?>" name="sort" id="sort_val">
+         <input type="hidden" value="" name="search" id="search_val">
+         <input type="hidden" value="" name="filter" id="filter_val">
+         <input type="hidden" value="" name="sort" id="sort_val">
          <input class="r3 c2 inputTextModal" placeholder="Введите заголовок задачи..." name="title" required>
          <textarea class="r5 c2 textarea" name="description" placeholder="Введите описание задачи..."></textarea>
          <input type="submit" class="r7 c2 inputSubmitModal w32" value="СОХРАНИТЬ">
